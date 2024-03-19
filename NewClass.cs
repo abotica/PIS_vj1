82d51d0811b1facddb474901fc0a4d6c145eff18
P3tra5 <petra@svilovic.com>
2024-03-19 19:17:27 +0100

    Prazna metoda

--- NewClass.cs
+++ NewClass.cs
@@ -4,9 +4,17 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace ConsoleApp7
 {
     internal class NewClass
     {
+        static void NewMethod()
+        {
+
+        }
     }
+
+    
+
 }
